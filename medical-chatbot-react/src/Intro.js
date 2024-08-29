496b7a2b7ae8d4f77995f3152cb44a78f44f9da7
Vanshikagoyal25 <vanshikag2501@gmail.com>
2024-08-29 15:34:35 +0530

    login route

--- medical-chatbot-react/src/Intro.js
+++ medical-chatbot-react/src/Intro.js
@@ -1,12 +1,8 @@
-import { motion } from 'framer-motion';
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { motion } from "framer-motion";
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Intro() {
-
-
-
   const container = {
     hidden: {
       opacity: 0,
@@ -22,74 +18,75 @@ function Intro() {
     },
   };
 
-
-
-
-
   return (
     <div id="services">
       <div className="container">
-        <motion.h1 whileInView={{ x: 0, opacity: 1 }}
+        <motion.h1
+          whileInView={{ x: 0, opacity: 1 }}
           initial={{ x: -300, opacity: 0 }}
-          transition={{ duration: 2 }} className="sub-title">Quick Guide</motion.h1>
-        <div className="services-list" >
+          transition={{ duration: 2 }}
+          className="sub-title"
+        >
+          Quick Guide
+        </motion.h1>
+        <div className="services-list">
+          <Link to="/Login">
+            <motion.div
+              initial={{ scale: 0 }}
+              whileInView={{ scale: 1 }}
+              transition={{ duration: 0.3 }}
+              >
+              <i className="fa-solid fa-right-to-bracket"></i>
+              <h2>Login/Signup</h2>
+              <p>Sign in or Login into your account</p>
+              <a href="#">learn more</a>
+            </motion.div>
+          </Link>
 
-          <motion.div initial={{ scale: 0 }}
+          <motion.div
+            initial={{ scale: 0 }}
             whileInView={{ scale: 1 }}
-            transition={{ duration: 0.3 }}>
-            <i className="fa-solid fa-right-to-bracket"></i>
-            <h2>Login/Signup</h2>
-            <p>Sign in or Login into your account</p>
-            <a href="#">learn more</a>
-          </motion.div>
-          <motion.div initial={{ scale: 0 }}
-            whileInView={{ scale: 1 }}
-            transition={{ duration: 0.5 }}>
+            transition={{ duration: 0.5 }}
+          >
             <i className="fa-solid fa-pen"></i>
             <h2>Medical History</h2>
             <p>Enter your Medical History and Age</p>
             <a href="#">learn more</a>
           </motion.div>
-          <motion.div initial={{ scale: 0 }}
+          <motion.div
+            initial={{ scale: 0 }}
             whileInView={{ scale: 1 }}
-            transition={{ duration: 0.7 }}>
+            transition={{ duration: 0.7 }}
+          >
             <i className="fa-brands fa-dochub"></i>
             <h2>Upload your Report</h2>
             <p>Upload your medical Report</p>
             <a href="#">learn more</a>
           </motion.div>
-          <motion.div initial={{ scale: 0 }}
+          <motion.div
+            initial={{ scale: 0 }}
             whileInView={{ scale: 1 }}
-            transition={{ duration: 0.9 }}>
+            transition={{ duration: 0.9 }}
+          >
             <i className="fa-brands fa-readme"></i>
             <h2>Analyze</h2>
             <p>Write Command Analyze in the chat box and ask any queries </p>
             <a href="#">learn more</a>
           </motion.div>
-
-
         </div>
-
       </div>
 
-      <div className='ip'>
-          <Link to="/Login">
-        <input
-          className='inputButtonp'
-          type="button"
-          //onClick={handleLogin}
-          value= 'Continue'
-        />
+      <div className="ip">
+        <Link to="/Login">
+          <input
+            className="inputButtonp"
+            type="button"
+            //onClick={handleLogin}
+            value="Continue"
+          />
         </Link>
-        </div>
-
+      </div>
     </div>
-
-
-
-
-
-
   );
 }
 
